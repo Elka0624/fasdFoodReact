f51d0c744698ade8168491dc75eaab917318d789
Elyor <ilhomovelyor20010624@gmail.com>
2023-11-25 16:55:52 +0500

    hozir qildim

---
+++ src/sections/Testimonial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/Testimonial.css'
+
+const Testimonial = () => {
+  return (
+    <div className='container'>
+      
+    </div>
+  )
+}
+
+export default Testimonial
