b892b3ec8a720e652d7f929fea5c6fc8ab3a346d
Elyor <ilhomovelyor20010624@gmail.com>
2023-11-25 16:35:36 +0500

    navbar and header complite

---
+++ src/sections/Feature.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import './styles/Feature.css';
+import fedex from '../images/fedEx.svg';
+import amazon from '../images/amazon.svg';
+import fedexx from '../images/fedExx.svg';
+import google from '../images/google.svg';
+import microsoft from '../images/microsoft.svg';
+import group from '../images/feature__top_-img.svg';
+import downloadIcon from '../images/downloadIcon.svg';
+import onClickIcon from '../images/onclick.svg';
+import inboxBtn from '../images/Group 429.svg';
+import imgOne from '../images/Group 463.svg';
+
+const Feature = () => {
+  return (
+    <div className='container'>
+      <div className="feature__top">
+        <img src={fedex} alt="" />
+        <img src={amazon} alt="" />
+        <img src={fedexx} alt="" />
+        <img src={google} alt="" />
+        <img src={microsoft} alt="" />
+      </div>
+      <div className="feature__body">
+        <div className="feature__body__left">
+          <img src={group} alt="" width={'700px'}/>
+        </div>
+        <div className="feature__body__right">
+          <p>Lorem ipsum dolor</p>
+          <h1>Two Saas are better than one.</h1>
+          <p>One of the most fundamental ways to go about that is to create a memorable e-commerce slogan.</p>
+          <div className="feature__info">
+            <div className="oneDiv">
+              <img src={downloadIcon} alt="" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+            <div className="oneDiv">
+              <img src={onClickIcon} alt="" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+            <div className="oneDiv">
+              <img src={downloadIcon} alt="" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="feature__bottom">
+        <div className="feature__bottom__left">
+          <p>Lorem ipsum dolor</p>
+          <h1>With a name like Saas, it has to be good.</h1>
+          <div className="inbox">
+            <p>With a name like Saas, it has to be good.</p>
+            <img src={inboxBtn} alt="" />
+          </div>
+          <div className="inbox">
+            <p>With a name like Saas, it has to be good.</p>
+            <img src={inboxBtn} alt="" />
+          </div>
+          <div className="inbox">
+            <p>With a name like Saas, it has to be good.</p>
+            <img src={inboxBtn} alt="" />
+          </div>
+          <div className="inbox">
+            <p>With a name like Saas, it has to be good.</p>
+            <img src={inboxBtn} alt="" />
+          </div>
+          <div className="inbox">
+            <p>With a name like Saas, it has to be good.</p>
+            <img src={inboxBtn} alt="" />
+          </div>
+          <div className="feature__btn__group">
+            <button>free trial</button>
+            <button>read more</button>
+          </div>
+        </div>
+        <div className="feature__bottom__right">
+          <img src={imgOne} alt="" width={'700px'} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Feature
---
+++ src/sections/Header.js
@@ -0,0 +1,25 @@
+import './styles/Header.css';
+import img from '../images/header-img.png';
+
+export default function Header() {
+    return (
+        <div style={{background: '#002547'}}>
+            <header className='container'>
+            <div className='header__left'>
+                <p>Lorem ipsum dolor</p>
+                <h1>
+                    Always the <span className='blueSpan'>real</span> <br /> thing, always <span className='blueSpan' style={{textDecorationLine: 'none'}}>Saas</span>
+                </h1>
+                <p>
+                    And that number is growing every day. <br /> That means <span style={{color: 'white', textDecorationLine: 'underline'}}>e-commerce</span> is thriving.
+                </p>
+                <button> Try for free</button>
+                <button> Try for free</button>
+            </div>
+            <div className='header__right'>
+                <img src={img} alt="" className='header__right__img' />
+            </div>
+        </header>
+        </div>
+    )
+}
